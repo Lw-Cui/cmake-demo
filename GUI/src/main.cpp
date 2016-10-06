d243ec36c5f766d632904d4d5ead428cdc13e437
Liwei <cui@hellolw.com>
2016-10-06 13:12:15 +0800

    使用 easylogging 日志库；添加相应的日志打印

--- GUI/src/main.cpp
+++ GUI/src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <interpreter.h>
 #include <gui.h>
+#include <easylogging++.h>
 
-int main() {
+INITIALIZE_EASYLOGGINGPP
+
+int main(int argc, char *argv[]) {
+    START_EASYLOGGINGPP(argc, argv);
+
     int i = foo();
     std::cout << str() << std::endl << i << std::endl;
+    LOG(INFO) << "Finish.";
     return 0;
 }
