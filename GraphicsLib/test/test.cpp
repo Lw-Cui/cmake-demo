7466a907aa1d398716c017a6d38bbfd90cbcc5e7
Liwei <cui@hellolw.com>
2016-10-06 13:22:40 +0800

    在单元测试中引入easylogging 日志库以通过编译

--- GraphicsLib/test/test.cpp
+++ GraphicsLib/test/test.cpp
@@ -1,6 +1,8 @@
 #include <internal.h>
 #include <gtest/gtest.h>
+#include <easylogging++.h>
 
+INITIALIZE_EASYLOGGINGPP
 TEST(GraphicsLib, SimpleTest) {
     ASSERT_EQ(func(), 0);
 }
--- Interpreter/test/test.cpp
+++ Interpreter/test/test.cpp
@@ -1,6 +1,8 @@
 #include <interpreter.h>
 #include <gtest/gtest.h>
+#include <easylogging++.h>
 
+INITIALIZE_EASYLOGGINGPP
 TEST(Interpreter, SimpleTest) {
     ASSERT_EQ(foo(), 5);
 }
