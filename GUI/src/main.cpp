32de42798c6ce6cdba81ea100b91a0eeb5482e63
Liwei <cui@hellolw.com>
2016-10-06 15:53:28 +0800

    使用 SFML 图形库

--- GUI/src/main.cpp
+++ GUI/src/main.cpp
@@ -2,6 +2,7 @@
 #include <interpreter.h>
 #include <gui.h>
 #include <easylogging++.h>
+#include <SFML/Graphics.hpp>
 
 INITIALIZE_EASYLOGGINGPP
 
@@ -10,6 +11,23 @@ int main(int argc, char *argv[]) {
 
     int i = foo();
     std::cout << str() << std::endl << i << std::endl;
-    LOG(INFO) << "Finish.";
+    LOG(INFO) << "Drawing...";
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
