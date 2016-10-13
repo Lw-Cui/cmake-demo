0c57d7ae42b4cc5a72693dc0e4275c3821c78d99
nightLei <zhileichen2015@gmail.com>
2016-10-13 23:05:38 +0800

    google test
    a simple google test !

--- Interpreter/test/test.cpp
+++ Interpreter/test/test.cpp
@@ -4,5 +4,11 @@
 
 INITIALIZE_EASYLOGGINGPP
 TEST(Interpreter, SimpleTest) {
-    ASSERT_EQ(foo(), 5);
+    ASSERT_EQ(5, foo());
+}
+TEST(Interpreter_mymin, test_succes){
+    EXPECT_EQ(1, mymin(1,2));
+}
+TEST(Interpreter_mymin, test_failure){
+    EXPECT_EQ(2, mymin(1,2));
 }
