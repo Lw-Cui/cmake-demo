88a6110433ac2c6aa7c74bbde4196e47c92adcf2
Liwei <cui@hellolw.com>
2016-10-18 11:13:43 +0800

    add Google Mock test

--- GraphicsLib/header/internal.h
+++ GraphicsLib/header/internal.h
@@ -2,4 +2,11 @@
 #define TEST2_INTERNAL_H
 int func();
 
+class Tmp {
+public:
+    virtual ~Tmp()  {}
+    Tmp() {}
+    virtual void foo(int) = 0;
+};
+
 #endif //TEST2_INTERNAL_H
--- GraphicsLib/test/test.cpp
+++ GraphicsLib/test/test.cpp
@@ -1,8 +1,22 @@
 #include <internal.h>
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include <easylogging++.h>
 
 INITIALIZE_EASYLOGGINGPP
+
+class MockTmp {
+public:
+    MOCK_METHOD1(foo, void(int));
+};
+
 TEST(GraphicsLib, SimpleTest) {
     ASSERT_EQ(func(), 0);
+}
+
+TEST(GraphicsLib, MockTest) {
+    MockTmp tmp;
+    EXPECT_CALL(tmp, foo(4)).Times(testing::AtLeast(2));
+    tmp.foo(4);
+    tmp.foo(4);
 }
